2f1b100ce8e2bc787a5cd2a36ccfef687a7dfc57
cukhoaimon <phucmapcaumieu@gmail.com>
2025-11-10 23:57:57 +0700

    update engine to support more

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,3 +1,4 @@
+import math
 
 class Value:
     def __init__(self, data, _children=(), _op='', label=''):
@@ -12,6 +13,7 @@ class Value:
         return f"Value(data={self.data}, op={self._op})"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
@@ -21,7 +23,11 @@ class Value:
         out._backward = _backward
         return out
 
+    def __radd__(self, other):
+        return self.__add__(other)
+
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
@@ -30,6 +36,54 @@ class Value:
 
         out._backward = _backward
 
+        return out
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    """
+        f(x) = x^n 
+        f'(x) = n.x'.x^(n-1)
+    """
+    def __pow__(self, power):
+        assert isinstance(power, (int, float))
+        out = Value(self.data**power, (self,), f"^{power}")
+
+        def _backward():
+            self.grad += power * out.grad * (self.data ** power - 1)
+
+        self._backward = _backward
+
+        return out
+
+    def __truediv__(self, other):
+        return self * other ** -1
+
+    def __neg__(self): # -self
+        return self * (-1)
+
+    def __sub__(self, other):
+        out = self + (-other)
+        out._op = '-'
+        return out
+
+    """
+        L(x)  = e^x
+        L'(x) = dL/dx = d(e^x)/dx = e^x
+
+        L(1)  = e^1
+        L'(1) = L(1) = e^1
+
+        -> self = x
+        -> out  = e^x
+    """
+    def exp(self):
+        out = Value(math.exp(self.data), (self,), _op='exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+
+        self._backward = _backward
         return out
 
     def backward(self):
