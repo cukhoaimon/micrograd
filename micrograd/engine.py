eb6b6052753f87276c6fe244e9fbeead49813c15
cukhoaimon <phucmapcaumieu@gmail.com>
2025-11-10 16:03:25 +0700

    update engine

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -6,15 +6,44 @@ class Value:
         self._op = _op
         self.grad = 0
         self.label = label
+        self._backward = lambda: None
 
     def __repr__(self):
         return f"Value(data={self.data}, op={self._op})"
 
     def __add__(self, other):
-        return Value(self.data + other.data, (self, other), '+')
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
+        return out
 
     def __mul__(self, other):
-        return Value(self.data * other.data, (self, other), '*')
+        out = Value(self.data * other.data, (self, other), '*')
 
-    def __sub__(self, other):
-        return Value(self.data - other.data, (self, other), '-')
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        # go one variable at a time and apply the chain rule to get its gradient
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
